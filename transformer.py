2f29ddbcfd796d37f19a37a830c66acde0a67127
Milind Shyani <mshyani@amazon.com>
2024-05-08 02:33:29 +0000

    added more to decoder block

--- transformer.py
+++ transformer.py
@@ -1,17 +1,43 @@
 from utils import *
 
 class DecoderLayer(nn.Module):
-    def __init__(self,hidden_dim):
+    def __init__(self,hidden_dim, num_heads, maxlen = 512, drop = 0.2):        
         super().__init__()
-        self.hidden_dim = hidden_dim        
-        self.layer = nn.Linear(hidden_dim, 4*hidden_dim)
+        self.hidden_dim = hidden_dim     
+        self.num_heads = num_heads
+        self.maxlen = maxlen
+        self.head_dim = hidden_dim // num_heads   
+        assert (hidden_dim % num_heads == 0)
+
+        self.ln1 = nn.LayerNorm(self.hidden_dim)
+        self.ln2 = nn.LayerNorm(self.hidden_dim)
+        self.Wq = nn.Linear(hidden_dim, hidden_dim, bias = False) 
+        self.Wk = nn.Linear(hidden_dim, hidden_dim, bias = False) 
+        self.Wv = nn.Linear(hidden_dim, hidden_dim, bias = False) 
+        self.Wz = nn.Linear(hidden_dim, hidden_dim, bias = False) 
+        self.fc = nn.Linear(hidden_dim, hidden_dim) 
+        self.register_buffer("attention_mask", torch.tril(torch.ones(maxlen,maxlen).view(-1,maxlen.maxlen)))
 
 
-    def attention(self):
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_dim, 4*hidden_dim),
+            nn.ReLU(),
+            nn.Linear(4*hidden_dim, hidden_dim) 
+        )
+        self.dropout = nn.Dropout(drop)
+
+    def attention(self,x):
+        # x has shape (B,L,d)
+        # Create attention mask that is (B,L,L)
+        prods.masked_fill(self.attention_mask == 0, float("-inf"))
+        
+
+
+
         pass
 
-    def mlp(self):
-        pass
+    def MLP(self,x):
+        return self.mlp(x)
 
     def forward(self,x):
         pass
