1582c2d84a210c2ebaa37854b11dc7c75b340b1d
Milind Shyani <milindshyani@gmail.com>
2024-05-09 05:27:27 +0000

    working skeleton

--- transformer.py
+++ transformer.py
@@ -29,14 +29,24 @@ class DecoderLayer(nn.Module):
     def attention(self,x):
         # x has shape (B,L,d)
         B,L,d = x.shape
+        
+        q = self.Wq(x)
+        q = einops.rearrange(q,"B L (n h)->B n L h",h=self.head_dim) 
 
-        q = self.Wq(x).view(B,L,self.num_heads, self.hidden_dim).transpose(1,2) 
-        k = self.Wk(x).view(B,L,self.num_heads, self.hidden_dim).transpose(1,2) 
-        v = self.Wv(x).view(B,L,self.num_heads, self.hidden_dim).transpose(1,2) 
+        k = self.Wk(x)
+        k = einops.rearrange(k,"B L (n h)->B n L h",h=self.head_dim) 
+
+        v = self.Wv(x)
+        v = einops.rearrange(v,"B L (n h)->B n L h",h=self.head_dim) 
+
+
+        # q = self.Wq(x).view(B,L,self.num_heads, self.head_dim).transpose(1,2) 
+        # k = self.Wk(x).view(B,L,self.num_heads, self.head_dim).transpose(1,2) 
+        # v = self.Wv(x).view(B,L,self.num_heads, self.head_dim).transpose(1,2) 
 
         # q k v has dimension (B, num_heads , L , hidden_dim)
 
-        attn = torch.einsum("blnh, bLnh -> bnlL" ,q,k) # This multiplication has time complexity B*num_heads*hidden_dim*L^2
+        attn = torch.einsum("bnlh, bnLh -> bnlL" ,q,k) # This multiplication has time complexity B*num_heads*hidden_dim*L^2
 
         # attn has shape (B,num_heads,L,L)
 
@@ -47,18 +57,19 @@ class DecoderLayer(nn.Module):
         # Where the first l is for q and the second for k. That is what we sum over
         
         # Create attention mask that is (B,num_heads,L,L)
-        attn.masked_fill(self.attention_mask == 0, float("-inf"))
-
+        print(attn.shape, self.attention_mask[:,:,:L,:L].shape)
+        attn.masked_fill(self.attention_mask[:,:,:L,:L] == 0, float("-inf"))
+        
         attn = attn / math.sqrt(d)
-
         attn_values = F.softmax(attn,dim=-1)        
 
         # attn_value has shape (B,num_heads,L,L)
         # v has shape (B,num_heads,L,hidden_dim)
         out = torch.einsum("bnlL,bnLd -> bnld",attn_values,v)
         # out has shape (B,num_heads,L, hidden_dim), where we have summer over keys as before
+        out = einops.rearrange(out,"b n L d-> b L (n d)")
         out = self.fc(out)
-        return
+        return out
 
     def MLP(self,x):
         return self.mlp(x)
@@ -76,10 +87,13 @@ class DecoderLayer(nn.Module):
 class positional(nn.Module):
         def __init__(self,max_len, d) -> None:
             super().__init__()            
-            denom = torch.exp( 2* torch.arange(0,d//2,1) * (-math.log(1e5)/d) )
+            denom = torch.exp( 2 * torch.arange(0,d//2,1) * (-math.log(1e5)/d) ).unsqueeze(0)
+
+            # denom has shape (1,d//2)
+                        
             pe = torch.zeros(max_len,d).unsqueeze(0)
-            pe[:,:,::2] = torch.sin(denom*torch.arange(self.max_len))
-            pe[:,:,1::2] = torch.sin(denom*torch.arange(self.max_len))
+            pe[0,:,::2] = torch.sin(denom*torch.arange(max_len).unsqueeze(1))
+            pe[0,:,1::2] = torch.cos(denom*torch.arange(max_len).unsqueeze(1))
             self.register_buffer("pe",pe)
 
         def forward(self,x):
@@ -88,13 +102,13 @@ class positional(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self,num_layers, hidden_dim, num_heads,max_len = 512):
+    def __init__(self,num_layers, hidden_dim, num_heads,vocab_size, max_len = 512):
         super().__init__()
         self.num_layers = num_layers
         self.num_heads = num_heads
         self.hidden_dim = hidden_dim
         self.emin = nn.Embedding(vocab_size,hidden_dim)
-        self.emout = nn.Embedding(hidden_dim,vocab_size)
+        self.emout = nn.Linear(hidden_dim,vocab_size)
         self.stack = nn.ModuleList([DecoderLayer(hidden_dim,num_heads) for _ in range(num_layers)])
         self.max_len = max_len
         self.pos = positional(max_len,hidden_dim)
@@ -111,6 +125,7 @@ class Decoder(nn.Module):
     
     def forward(self,x):        
         x = self.embed(x)
+        print("after embed shape",x.shape)
         for layer in self.stack:
             x = layer(x)
         x = self.emout(x)
@@ -118,9 +133,22 @@ class Decoder(nn.Module):
     
 
         
-class Transformer():
-    def __init__(self) -> None:
-        super().__init__()
+class GPT(nn.Module):
+    def __init__(self,num_layers,hidden_dim,num_heads,max_len=512) -> None:
+        super().__init__()        
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        vocab_size = self.tokenizer.vocab_size
+        self.decoder = Decoder(num_layers,hidden_dim,num_heads,vocab_size,max_len)
 
     def forward(self,x):
-        pass
+        x = self.tokenizer(x,return_tensors="pt")["input_ids"]        
+        print("YOYOYOYO",x)
+        x = self.decoder(x)
+        return x
+
+
+
+if __name__ == "__main__":
+    gpt = GPT(2,64,8)
+    output = gpt(["quick brown fox fox","I like Physics fox"])
+    print(output.shape)
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
 import math
+from transformers import BertTokenizer, BertModel
+import einops
+
 
 
 
