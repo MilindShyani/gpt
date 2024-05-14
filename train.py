fixed error during training; was doing softmax twice

train.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     parser.add_argument("-fp", "--file_path",type = str)    
     args = parser.parse_args()
     mymodel = GPT(args.num_layers, args.embed_dim, args.num_heads)
-    optimizer = optim.Adam(mymodel.parameters(),lr=1e-5)    
+    optimizer = optim.Adam(mymodel.parameters(),lr=1e-3)    
     
     loss = nn.CrossEntropyLoss(reduction="none")
     with open(args.file_path,"rb") as f:

transformer.py
@@ -140,9 +140,9 @@ class GPT(nn.Module):
         return x
                                                                                
     def forward(self, s):
-        x = self.forward_pass(s)
-        x = F.softmax(x,-1)                                                     
+        x = self.forward_pass(s)                                                             
         if not self.train: 
+            x = F.softmax(x,-1)
             samples = torch.multinomial(einops.rearrange(x,"B L D -> (B L) D"),num_samples=1)
             samples = einops.rearrange(samples,"(B L) p -> B L p", B = len(s)).squeeze()   
             out = self.tokenizer.batch_decode(samples)         