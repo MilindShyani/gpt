7080a34c7710a7bc78937b43bb3b1ff044029d05
Milind Shyani <milinshyani@gmail.com>
2024-07-15 00:33:33 +0000

    more ddp

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ def train(model,loss,optimizer,tokens,batch_size=4,ddp=False):
     train_loss = []
     input_ids, attention_mask = tokens["input_ids"], tokens["attention_mask"]
     if ddp:
+        print("Lets use ddp")
         assert torch.cuda.is_available()
         init_process_group(backend="nccl")
         ddp_rank = int(os.environ["RANK"])
@@ -32,11 +33,12 @@ def train(model,loss,optimizer,tokens,batch_size=4,ddp=False):
     mega_batch = 2**10
     assert mega_batch % (batch_size*ddp_world_size) == 0
     accum_steps  = mega_batch // (batch_size*ddp_world_size)
-    mymodel = DDP(mymodel)
-    rawmodel = mymodel.module if ddp else mymodel
-
+    model.to(device)
+    model = DDP(model,device_ids=[ddp_local_rank])
+    rawmodel = model.module if ddp else model
+    loss_sofar = 0
     for epoch in range(num_epochs):
-        torch.save(mymodel,f"model_{model.num_heads}_{model.num_layers}_{model.hidden_dim}_{epoch}.pt")
+        # torch.save(model,f"model_{model.num_heads}_{model.num_layers}_{model.hidden_dim}_{epoch}.pt")
         # for i in tqdm(range(0,len(input_ids),batch_size)):
         for i in range(ddp_local_rank*batch_size,len(input_ids),batch_size*ddp_world_size):
             optimizer.zero_grad()
@@ -47,28 +49,32 @@ def train(model,loss,optimizer,tokens,batch_size=4,ddp=False):
                 input_batch = input_ids[start_at:end_at].to(device)
                 attn_batch = attention_mask[start_at:end_at].to(device) 
 
-                with torch.autocast(device_type= model.device, dtype = torch.float32):
-                    output = model.forward(input_batch,attn_batch)
+                
+                output = model.forward(input_batch,attn_batch)
             
-                tokens_onehot = F.one_hot(input_batch,num_classes = model.vocab_size).to(torch.float)
+                tokens_onehot = F.one_hot(input_batch,num_classes = model.module.vocab_size).to(torch.float)
                 # output has shape (B,L,V). The shift in targets is for next token prediction obviously
                 # So does tokens_onehot
                 batch_loss = loss(output[:,:-1,:].transpose(1,2),tokens_onehot[:,1:].transpose(1,2))
                 # batch_loss has dimensions (B,L-1)
                 batch_loss = batch_loss*attn_batch[:,1:] # double check this 1: vs :-1
                 batch_loss = (1/accum_steps)*torch.mean(batch_loss)
-
-            batch_loss.backward()
+                loss_sofar += batch_loss.detach()
+                if step == accum_steps - 1 and ddp:
+                    model.require_backward_grad_sync = True                    
+                batch_loss.backward()
+            if ddp:
+                dist.all_reduce(loss_sofar,op=dist.ReduceOp.AVG)
             optimizer.step()        
-            train_loss.append(batch_loss.item())  
-            print(batch_loss.item())  
+            train_loss.append(loss_sofar.item())  
+            print(loss_sofar.item())  
             if not torch.isfinite(batch_loss):
                 raise Exception("Loss gone crazy")
             if i % 500 == 0:          
                 plt.plot(train_loss)
                 plt.savefig("train_loss")
                 plt.show()
-    torch.save(mymodel,f"model_{model.num_heads}_{model.num_layers}_{model.hidden_dim}_final.pt")
+    # torch.save(mymodel,f"model_{model.num_heads}_{model.num_layers}_{model.hidden_dim}_final.pt")
     return model
                 
 if __name__ == "__main__":
